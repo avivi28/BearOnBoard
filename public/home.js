781652c6327c1215889b11aa60bbf676cadb8241
= <avivichan.ccsu67@gmail.com>
2022-05-11 19:01:54 +0800

    Add: addpost() modal outlook & button

--- public/home.js
+++ public/home.js
@@ -58,6 +58,7 @@ function initMap() {
 					position: pos,
 					map: map,
 					icon: image,
+					animation: google.maps.Animation.BOUNCE,
 				});
 
 				map.setCenter(pos);
@@ -70,4 +71,78 @@ function initMap() {
 		// Browser doesn't support Geolocation
 		handleLocationError(false, infoWindow, map.getCenter());
 	}
+
+	// Create the search box and link it to the UI element.
+	const input = document.getElementById('pac-input');
+	const searchBox = new google.maps.places.SearchBox(input);
+
+	map.push(input);
+	// Bias the SearchBox results towards current map's viewport.
+	map.addListener('bounds_changed', () => {
+		searchBox.setBounds(map.getBounds());
+	});
+
+	let markers = [];
+
+	// Listen for the event fired when the user selects a prediction and retrieve
+	// more details for that place.
+	searchBox.addListener('places_changed', () => {
+		const places = searchBox.getPlaces();
+
+		if (places.length == 0) {
+			return;
+		}
+
+		// Clear out the old markers.
+		markers.forEach((marker) => {
+			marker.setMap(null);
+		});
+		markers = [];
+
+		// For each place, get the icon, name and location.
+		const bounds = new google.maps.LatLngBounds();
+
+		places.forEach((place) => {
+			if (!place.geometry || !place.geometry.location) {
+				console.log('Returned place contains no geometry');
+				return;
+			}
+
+			const icon = {
+				url: place.icon,
+				size: new google.maps.Size(71, 71),
+				origin: new google.maps.Point(0, 0),
+				anchor: new google.maps.Point(17, 34),
+				scaledSize: new google.maps.Size(25, 25),
+			};
+
+			// Create a marker for each place.
+			markers.push(
+				new google.maps.Marker({
+					map,
+					icon,
+					title: place.name,
+					position: place.geometry.location,
+				})
+			);
+			if (place.geometry.viewport) {
+				// Only geocodes have viewport.
+				bounds.union(place.geometry.viewport);
+			} else {
+				bounds.extend(place.geometry.location);
+			}
+		});
+		map.fitBounds(bounds);
+	});
+}
+
+window.initMap = initMap;
+
+//----------------Posts Modal---------------
+const modal = document.getElementById('modal');
+function addPost() {
+	modal.style.display = 'block';
+}
+function hidePost() {
+	modal.style.display = 'none';
 }
